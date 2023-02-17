f9de99aa5a596ca53b4ef8ea8adebe091afa1b86
JigmeT123 <tashinamgyaljigme@gmail.com>
2023-02-17 13:12:34 +0400

    fix(): rubocop fixes

--- spec/graphql/resolvers/start_spec.rb
+++ spec/graphql/resolvers/start_spec.rb
@@ -4,10 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Resolvers::Game::Start do
   describe '.resolve' do
-
     context 'when the user starts the game' do
       it 'returns result of the game' do
-
       end
     end
   end
