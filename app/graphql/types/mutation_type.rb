a8b572978435c4927c035b0337e57d3e076ad9c9
JigmeT123 <tashinamgyaljigme@gmail.com>
2023-02-17 11:20:39 +0400

    feat(): include modules

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,9 +2,11 @@
 
 module Types
   class MutationType < Types::BaseObject
+    include MountMutation
+
     # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
+    field :test_field, String, null: false, description: 'An example field added by the generator'
+
     def test_field
       'Hello World'
     end
