5831f637e6f4a7c3e8ead92b65d8bc9d38c41477
JigmeT123 <tashinamgyaljigme@gmail.com>
2023-02-17 11:36:02 +0400

    feat(): resolver for game

--- app/graphql/resolvers/game/start.rb
+++ app/graphql/resolvers/game/start.rb
@@ -4,6 +4,17 @@ module Resolvers
   module Game
     class Start < GraphQL::Schema::Resolver
       graphql_name 'StartGame'
+
+      class ResultAttributes < GraphQL::Schema::Object
+        field :result, String, null: false
+      end
+
+      argument :user_choice, Types::GameEnum, null: false
+      type ResultAttributes, null: false
+
+      def resolve(user_choice:)
+        # TODO: Game Logic
+      end
     end
   end
 end
